83ab98b4eb51c6d24e39ab090d21291b8ee4d3da
SomebodyWhoUsesGithub <130918090+SomebodyWhoUsesGithub@users.noreply.github.com>
2024-01-01 03:40:23 +0000

    ac

--- public/js/eventListeners.js
+++ public/js/eventListeners.js
@@ -1,30 +1,40 @@
+var lastMove = 0;
+document.addEventListener('mousemove', function() {
+    // do nothing if last move was less than 40 ms ago
+
+});
 addEventListener('click', (event) => {
-  if (players[socket.id]) {
-    const playerPosition = {
-      x: players[socket.id].x,
-      y: players[socket.id].y
-    }
-    const angle = Math.atan2(
-      event.clientY * window.devicePixelRatio - playerPosition.y,
-      event.clientX * window.devicePixelRatio - playerPosition.x
-    )
-    // const velocity = {
-    //   x: Math.cos(angle) * 5,
-    //   y: Math.sin(angle) * 5
-    // }
-    socket.emit('shoot', {
-      x: playerPosition.x,
-      y: playerPosition.y,
-      angle
-    })
-    // projectiles.push(
-    //   new Projectile({
-    //     x: playerPosition.x,
-    //     y: playerPosition.y,
-    //     radius: 5,
-    //     color: 'gold',
-    //     velocity: velocity
-    //   })
-    // )
+  if(Date.now() - lastMove > 50) {
+      // Do stuff
+      lastMove = Date.now();
+      if (players[socket.id]) {
+        const playerPosition = {
+          x: players[socket.id].x,
+          y: players[socket.id].y
+        }
+        const angle = Math.atan2(
+          event.clientY * window.devicePixelRatio - playerPosition.y,
+          event.clientX * window.devicePixelRatio - playerPosition.x
+        )
+        // const velocity = {
+        //   x: Math.cos(angle) * 5,
+        //   y: Math.sin(angle) * 5
+        // }
+        socket.emit('shoot', {
+          x: playerPosition.x,
+          y: playerPosition.y,
+          angle
+        })
+        // projectiles.push(
+        //   new Projectile({
+        //     x: playerPosition.x,
+        //     y: playerPosition.y,
+        //     radius: 5,
+        //     color: 'gold',
+        //     velocity: velocity
+        //   })
+        // )
+      }
   }
+
 })
